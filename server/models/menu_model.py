ec0048abf65f9ed4c12d44a8c0cc9530a36b8597
RamyJamea <ramyjamea.ca@gmail.com>
2026-04-06 20:47:14 +0200

    feat (models): menu models

---
+++ server/models/menu_model.py
@@ -0,0 +1,40 @@
+from decimal import Decimal
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, UniqueConstraint, Numeric
+from .base_model import Base, AuditMixin
+from .catalog_model import Product, Size, Extra
+from .branch_model import Branch
+from .order_model import OrderItem
+
+
+class MenuItemExtra(Base, AuditMixin):
+    __tablename__ = "menu_items_extras"
+
+    menu_item_id: Mapped[int] = mapped_column(ForeignKey("menu_items.id"))
+    extra_id: Mapped[int] = mapped_column(ForeignKey("extras.id"))
+    price: Mapped[Decimal] = mapped_column(Numeric(10, 2))
+
+    menu_item: Mapped["MenuItem"] = relationship(back_populates="menu_items_extras")
+    extra: Mapped["Extra"] = relationship(back_populates="menu_item_extras")
+
+
+class MenuItem(Base, AuditMixin):
+    __tablename__ = "menu_items"
+    __table_args__ = (
+        UniqueConstraint(
+            "branch_id", "product_id", "size_id", name="uix_branch_product_size"
+        ),
+    )
+
+    branch_id: Mapped[int] = mapped_column(ForeignKey("branches.id"))
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"))
+    size_id: Mapped[int] = mapped_column(ForeignKey("sizes.id"))
+    price: Mapped[Decimal] = mapped_column(Numeric(10, 2))
+
+    branch: Mapped["Branch"] = relationship(back_populates="menu_items")
+    menu_items_extras: Mapped[list["MenuItemExtra"]] = relationship(
+        back_populates="menu_item"
+    )
+    product: Mapped["Product"] = relationship(back_populates="menu_items")
+    size: Mapped["Size"] = relationship(back_populates="menu_items")
+    order_items: Mapped[list["OrderItem"]] = relationship(back_populates="menu_item")
