4addaf3b3068838822ec847c34331bc49da83888
RamyJamea <ramyjamea.ca@gmail.com>
2026-04-06 23:58:58 +0200

    refactor (core): location -> branch schema

---
+++ server/core/schemas/branch_schema.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel, ConfigDict
+from datetime import datetime
+
+
+class BranchBase(BaseModel):
+    name: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class BranchCreate(BranchBase):
+    pass
+
+
+class BranchUpdate(BaseModel):
+    name: str | None = None
+
+
+class BranchRead(BranchBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+    deleted_at: datetime | None = None
+
+
+class TableBase(BaseModel):
+    num_chairs: int
+    is_available: bool = True
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class TableCreate(TableBase):
+    branch_id: int
+
+
+class TableUpdate(BaseModel):
+    num_chairs: int | None = None
+    is_available: bool | None = None
+
+
+class TableRead(TableBase):
+    id: int
+    branch_id: int
+    created_at: datetime
+    updated_at: datetime
+    deleted_at: datetime | None = None
