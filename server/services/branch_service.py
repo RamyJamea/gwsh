8241ac933e13fbe076f7df76ba787b7957e69fb2
RamyJamea <ramyjamea.ca@gmail.com>
2026-04-06 21:18:37 +0200

    feat (services): branch and table services

---
+++ server/services/branch_service.py
@@ -0,0 +1,39 @@
+from sqlalchemy.orm import Session
+from ..repositories import BranchRepository, RestaurantTableRepository
+from ..core.schemas import BranchCreate, BranchUpdate, TableCreate, TableUpdate
+from .base_service import BaseService
+
+
+class BranchService(BaseService):
+    def __init__(self, session: Session):
+        super().__init__(session, BranchRepository(session))
+
+    def create(self, data: BranchCreate):
+        obj = self.repo.create(data.model_dump())
+        self.session.commit()
+        return obj
+
+    def update(self, branch_id: int, data: BranchUpdate):
+        obj = self.get(branch_id)
+        self.repo.update(obj, data.model_dump(exclude_unset=True))
+        self.session.commit()
+        return obj
+
+
+class TableService(BaseService):
+    def __init__(self, session: Session):
+        super().__init__(session, RestaurantTableRepository(session))
+        self.branch_repo = BranchRepository(session)
+
+    def create(self, data: TableCreate):
+        if not self.branch_repo.get_by_id(data.branch_id):
+            raise ValueError("Branch not found")
+        obj = self.repo.create(data.model_dump())
+        self.session.commit()
+        return obj
+
+    def update(self, table_id: int, data: TableUpdate):
+        obj = self.get(table_id)
+        self.repo.update(obj, data.model_dump(exclude_unset=True))
+        self.session.commit()
+        return obj
