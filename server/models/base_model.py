e467a65393cacbe5f041a28f483282706d1cc300
RamyJamea <ramyjamea.ca@gmail.com>
2026-04-06 20:45:25 +0200

    feat (models): base model with main fields

---
+++ server/models/base_model.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from typing import Optional
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+
+
+class Base(DeclarativeBase): ...
+
+
+class AuditMixin:
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(), onupdate=func.now()
+    )
+    deleted_at: Mapped[Optional[datetime]] = mapped_column(nullable=True, default=None)
