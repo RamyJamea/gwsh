2aab3622eaa095d9488450f7e6814a2cd6b7b87e
RamyJamea <ramyjamea.ca@gmail.com>
2026-04-06 23:57:45 +0200

    feat (core): branch, catalog and menu service dependencies

--- server/core/dependencies/services_dependencies.py
+++ server/core/dependencies/services_dependencies.py
@@ -2,6 +2,9 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 from ..db import get_db
 from ...services import AuthenticationService, UserService
+from ...services import BranchService, TableService
+from ...services import CategoryService, ProductService, SizeService, ExtraService
+from ...services import MenuService
 
 
 def get_user_service(db: Session = Depends(get_db)) -> UserService:
@@ -12,3 +15,31 @@ def get_auth_service(
     user_service: UserService = Depends(get_user_service),
 ) -> AuthenticationService:
     return AuthenticationService(user_service)
+
+
+def get_branch_service(db: Session = Depends(get_db)) -> BranchService:
+    return BranchService(db)
+
+
+def get_table_service(db: Session = Depends(get_db)) -> TableService:
+    return TableService(db)
+
+
+def get_category_service(db: Session = Depends(get_db)) -> CategoryService:
+    return CategoryService(db)
+
+
+def get_product_service(db: Session = Depends(get_db)) -> ProductService:
+    return ProductService(db)
+
+
+def get_size_service(db: Session = Depends(get_db)) -> SizeService:
+    return SizeService(db)
+
+
+def get_extra_service(db: Session = Depends(get_db)) -> ExtraService:
+    return ExtraService(db)
+
+
+def get_menu_service(db: Session = Depends(get_db)) -> MenuService:
+    return MenuService(db)
