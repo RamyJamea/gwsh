578b7bf5e85773ee51ee471d5fa11e501952a8b5
RamyJamea <ramyjamea.ca@gmail.com>
2026-04-06 20:48:02 +0200

    feat (models): models packaging

---
+++ server/models/__init__.py
@@ -0,0 +1,7 @@
+from .history_model import OrderHistory, OrderHistoryItem, OrderHistoryItemExtra
+from .catalog_model import Product, Category, Extra, Size
+from .branch_model import Branch, RestaurantTable
+from .menu_model import MenuItemExtra, MenuItem
+from .order_model import Order, OrderItem, OrderItemExtra
+from .user_model import User
+from .base_model import Base
