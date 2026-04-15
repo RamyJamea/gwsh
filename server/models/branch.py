77d7400559cbfe550f7f05a5cf923fe39d9277b2
RamyJamea <ramyjamea.ca@gmail.com>
2026-04-15 11:36:19 +0200

    fix (orm): add is active and remove orphan deletion from branches so that breserve orders when branch is not needed

--- server/models/branch.py
+++ server/models/branch.py
@@ -1,29 +1,22 @@
 from typing import TYPE_CHECKING
-from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from .base import Base, AuditMixin
+from .base import Base, AuditMixin, ORPHAN
+from ..core.enums import TableEnum
 
 if TYPE_CHECKING:
-    from .user import User
+    from .user import UserModel
     from .menu_item_extra import MenuItem
     from .order import Order
     from .table import RestaurantTable
 
 
-class Branch(Base, AuditMixin):
-    __tablename__ = "branches"
+class BranchModel(Base, AuditMixin):
+    __tablename__ = TableEnum.BRANCHES.value
 
     name: Mapped[str] = mapped_column(unique=True, index=True)
+    is_active: Mapped[bool] = mapped_column(default=True)
 
-    tables: Mapped[list["RestaurantTable"]] = relationship(
-        back_populates="branch", cascade="all, delete-orphan"
-    )
-    users: Mapped[list["User"]] = relationship(
-        back_populates="branch", cascade="all, delete-orphan"
-    )
-    menu_items: Mapped[list["MenuItem"]] = relationship(
-        back_populates="branch", cascade="all, delete-orphan"
-    )
-    orders: Mapped[list["Order"]] = relationship(
-        back_populates="branch", cascade="all, delete-orphan"
-    )
+    tables: Mapped[list["RestaurantTable"]] = relationship(back_populates="branch", cascade=ORPHAN)
+    users: Mapped[list["UserModel"]] = relationship(back_populates="branch", cascade=ORPHAN)
+    menu_items: Mapped[list["MenuItem"]] = relationship(back_populates="branch", cascade=ORPHAN)
+    orders: Mapped[list["Order"]] = relationship(back_populates="branch")
